813d598854c6b5e02740c052540032fd4b61756c
Vadik <gloom16@mail.ru>
2024-08-20 15:57:51 +0300

    Реализация модели

---
+++ src/main/java/org/example/springbootrest/model/Customer.java
@@ -0,0 +1,28 @@
+package org.example.springbootrest.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "budget")
+    private BigDecimal budget;
+}
