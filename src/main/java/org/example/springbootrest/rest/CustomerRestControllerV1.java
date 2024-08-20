783db000df5edc55a9d56154b5f5a23dc7d0be89
Vadik <gloom16@mail.ru>
2024-08-20 16:41:17 +0300

    Добавил использование Dto

--- src/main/java/org/example/springbootrest/rest/CustomerRestControllerV1.java
+++ src/main/java/org/example/springbootrest/rest/CustomerRestControllerV1.java
@@ -1,6 +1,8 @@
 package org.example.springbootrest.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springbootrest.dto.CustomerDto;
+import org.example.springbootrest.mapper.CustomerMapper;
 import org.example.springbootrest.model.Customer;
 import org.example.springbootrest.service.CustomerService;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,7 @@ import java.util.List;
 @RequestMapping("/api/v1/customers")
 public class CustomerRestControllerV1 {
 
+    private final CustomerMapper customerMapper;
     private final CustomerService customerService;
 
     @GetMapping("/{id}")
@@ -32,12 +35,12 @@ public class CustomerRestControllerV1 {
     }
 
     @PostMapping("/{id}")
-    public ResponseEntity<Customer> customerResponseEntity(@RequestBody @Validated Customer customer) {
+    public ResponseEntity<Customer> saveNewCustomer(@RequestBody @Validated CustomerDto customer) {
         if (customer == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        customerService.save(customer);
-        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+        Customer saveCustomer = customerService.save(customerMapper.toEntity(customer));
+        return new ResponseEntity<>(saveCustomer, HttpStatus.CREATED);
     }
 
     @PutMapping
@@ -60,11 +63,12 @@ public class CustomerRestControllerV1 {
     }
 
     @GetMapping
-    public ResponseEntity<List<Customer>> getAllCustomers() {
+    public ResponseEntity<List<CustomerDto>> getAllCustomers() {
         List<Customer> customers = customerService.findAll();
         if (customers == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(customers, HttpStatus.OK);
+        List<CustomerDto> customerDto = customers.stream().map(customerMapper::toDto).toList();
+        return new ResponseEntity<>(customerDto, HttpStatus.OK);
     }
 }
